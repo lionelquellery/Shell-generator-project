e09a1007c0ce959ac4422c88b57f5b619ab5149d
lmarti17 <lucas.martin.78@free.fr>
2016-03-02 16:02:58 +0100

    Add sh file which clone lmarti17 starter-front repo

---
+++ ini-starter.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Lucas front-project starter
+#author : Lucas Martin
+#github: lmarti17
+#script use shell services
+
+echo "Salut $USER Welcome in Front-starter generator" 
+echo -e "Lucas install Shell: Nom du Projet"
+read name
+echo "project Name -> $name"
+
+cd ;
+cd Sites/
+
+git clone https://github.com/lmarti17/front-end-starter.git $name
+
+cd $name
+
+rm -rf .git
+git init
+npm install
+gulp
