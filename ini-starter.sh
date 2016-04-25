83cd4ab80d35ee3b60eb565a3be3f11dc503d0c2
lmarti17 <lucas.martin.78@free.fr>
2016-04-25 18:43:54 +0200

    Update script --> npm update after installing dependencies

--- ini-starter.sh
+++ ini-starter.sh
@@ -4,7 +4,7 @@
 #github: lmarti17
 #script use shell services
 
-echo "Salut $USER Welcome in Front-starter generator" 
+echo "Salut $USER Welcome in Front-starter generator"
 echo -e "Lucas install Shell: Nom du Projet"
 read name
 echo "project Name -> $name"
@@ -17,7 +17,6 @@ git clone https://github.com/lmarti17/front-end-starter.git $name
 cd $name
 
 rm -rf .git
-git init
-npm update
 npm install
+npm update
 gulp
